5e2cd59891b8ca43d208c497e4f4fdd7ab21eac6
Ruslan Diachenko <rd.ryly@gmail.com>
2024-08-05 21:38:48 +0100

    add lambda input

--- src/it/resources/com/google/checkstyle/test/chapter4formatting/rule451wheretobreak/InputSeparatorWrapMethodRef.java
+++ src/it/resources/com/google/checkstyle/test/chapter4formatting/rule451wheretobreak/InputSeparatorWrapMethodRef.java
@@ -21,4 +21,19 @@ public class InputSeparatorWrapMethodRef {
         String:: // violation ''::' should be on a new line.'
             compareToIgnoreCase);
   }
+
+  void anotherCase() {
+    java.util.function.BiFunction<String, Long, Long> r = (String label, Long value) -> { // OK
+      return value;
+    };
+
+    java.util.function.Predicate<String> predicate = str -> // OK
+            str.isEmpty();
+
+    java.util.function.Function<String, String> s = (String label) ->
+      // violation below ''{' at column 7 should be on the previous line.'
+      {
+        return label;
+      };
+  }
 }
